16128be59cce250707d14c7df147c0e14b19fccf
milajotic <mijoa058@osloskolen.no>
2026-05-29 11:31:56 +0200

    parameter greia

--- app.py
+++ app.py
@@ -5,5 +5,9 @@ app = Flask(__name__)
 def home():
     return "Hello World"
 
+@app.route('/produkt/<int:id>')
+def vis_produkt(id):
+    return f'Du ser på produkt nummer {id}'
+
 if __name__ == '__main__':
     app.run(debug=True)
